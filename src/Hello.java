17812aecf6e3219a95a5ed7a70fba8b9b6a82ecf
ronnilenvighansen <ronnilenvighansen@gmail.com>
2019-02-19 00:31:31 +0100

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Created by ronnilenvighansen on 18/02/2019.
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println("Hello World ... " + i);
+        }
+    }
+}
